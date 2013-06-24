0a8b354af67813e9cda6e3bfbe68616aab1db69c
Sean Baskin <seanbaskin@gmail.com>
2013-06-24 03:42:57 -0400

    Tabs to spaces

--- read-las-ascii-write-pcd.cpp
+++ read-las-ascii-write-pcd.cpp
@@ -11,26 +11,26 @@
 
 int main (int argc, char** argv)
 {
-	pcl::PointCloud<pcl::PointXYZ> cloud;
-	std::ifstream infile("145E.xyz");
-	int las_num_points;
-	std::string line = "";
-	std::string delims = ",";
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    std::ifstream infile("145E.xyz");
+    int las_num_points;
+    std::string line = "";
+    std::string delims = ",";
     std::vector<std::string> vector_las;
 
-	while (std::getline(infile, line))
-	{
-		boost::trim (line);
-		boost::split(vector_las, line, boost::is_any_of(delims), boost::token_compress_on);
-		cloud.push_back (pcl::PointXYZ (float (atof (vector_las[0].c_str ())), 
-										float (atof (vector_las[1].c_str ())), 
-										float (atof (vector_las[2].c_str ()))));
-	}
-	
-	cloud.width = cloud.size();
-	cloud.height = 1;
-	cloud.is_dense = true;
-	cloud.points.resize(cloud.width * cloud.height);
+    while (std::getline(infile, line))
+    {
+        boost::trim (line);
+        boost::split(vector_las, line, boost::is_any_of(delims), boost::token_compress_on);
+        cloud.push_back (pcl::PointXYZ (float (atof (vector_las[0].c_str ())), 
+                                        float (atof (vector_las[1].c_str ())), 
+                                        float (atof (vector_las[2].c_str ()))));
+    }
+    
+    cloud.width = cloud.size();
+    cloud.height = 1;
+    cloud.is_dense = true;
+    cloud.points.resize(cloud.width * cloud.height);
 
-	pcl::io::savePCDFileASCII ("145E.pcd", cloud);
+    pcl::io::savePCDFileASCII ("145E.pcd", cloud);
 }
